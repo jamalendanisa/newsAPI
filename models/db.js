2ce0d755f43c4ccb10644e9af3c8d9622c2d00e5
Anisa Jamalenda <anisa.jamalenda@gmail.com>
2019-12-18 01:43:51 +0700

    update db.js

--- models/db.js
+++ models/db.js
@@ -11,19 +11,11 @@ const mysql = require("mysql");
 
 // Create a connection to the database
 const connection = mysql.createConnection({
-  host: dbConfig.HOST,
-  user: dbConfig.USER,
-  password: dbConfig.PASSWORD,
-  database: dbConfig.DB
-}); 
-
-// Create a connection to the database
-// const connection = mysql.createConnection({
-//   host     : process.env.MYSQL_ADDON_HOST,
-//   database : process.env.MYSQL_ADDON_DB,
-//   user     : process.env.MYSQL_ADDON_USER,
-//   password : process.env.MYSQL_ADDON_PASSWORD
-// });
+  host     : process.env.MYSQL_ADDON_HOST,
+  database : process.env.MYSQL_ADDON_DB,
+  user     : process.env.MYSQL_ADDON_USER,
+  password : process.env.MYSQL_ADDON_PASSWORD
+});
 
 // open the MySQL connection and create database and table if doesn't exist
 connection.connect(error => {
