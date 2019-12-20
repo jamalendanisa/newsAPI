f179a3d5a69641f85b7bda0e5df67577d34ef9ab
Anisa Jamalenda <anisa.jamalenda@gmail.com>
2019-12-21 00:59:02 +0700

    Cors handling

--- app.js
+++ app.js
@@ -53,11 +53,10 @@ app.use(basicAuth({
 // app.use(Cors(corsOpt));
 // app.options('*', Cors(corsOpt));
 
-app.all('*', function(req, res, next) {
-       res.header("Access-Control-Allow-Origin", "*");
-       res.header("Access-Control-Allow-Headers", "X-Requested-With");
-       res.header('Access-Control-Allow-Headers', 'Content-Type');
-       next();
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 // parse requests of content-type: application/json
