6f951537618854a863427ef818a08e733b7e54e2
Anisa Jamalenda <anisa.jamalenda@gmail.com>
2019-12-26 07:28:26 +0700

    add video optimization

---
+++ build/precache-manifest.1ceadfcd98c3cab30792bbb061971019.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73a42ec36c44e49954a7be2becae9b0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43de668274138a6bac62",
+    "url": "/static/css/2.b9ddac8a.chunk.css"
+  },
+  {
+    "revision": "2b3d2b72cbff4f178f5c",
+    "url": "/static/css/main.acfc6fc5.chunk.css"
+  },
+  {
+    "revision": "43de668274138a6bac62",
+    "url": "/static/js/2.a525c457.chunk.js"
+  },
+  {
+    "revision": "65cfaa1e680eaf9e519001143e43c55b",
+    "url": "/static/js/2.a525c457.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2b3d2b72cbff4f178f5c",
+    "url": "/static/js/main.e713a6ac.chunk.js"
+  },
+  {
+    "revision": "691ca8fda51f2fe961ac",
+    "url": "/static/js/runtime-main.0d6ca171.js"
+  }
+]);
